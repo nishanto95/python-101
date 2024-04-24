1ecf4a3451979b7d6cd5506f32f77bb0ccd5497d
nishanto95 <167629690+nishanto95@users.noreply.github.com>
2024-04-24 17:59:10 +0530

    Add files via upload

---
+++ Numpy/array_comparison.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 24 17:56:52 2024
+
+@author: Nishant
+"""
+
+"""
+import numpy as np
+a = np.array([[[ 0, 0, 0],
+ [ 0, 0, 0],
+ [ 0, 0, 0],
+ [ 0, 0, 0]],
+[[ 0, 0, 255],
+ [255, 255, 255],
+ [ 0, 0, 0],
+ [255, 0, 0]]])
+b = np.array([255, 0, 255])
+c = np.all(a == b, axis = 1) 
+"""
+[[false false false],[false,false,false]]
