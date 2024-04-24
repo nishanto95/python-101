95baf8e3dac36d7eb5d5edb1023d49e672fc7503
nishanto95 <167629690+nishanto95@users.noreply.github.com>
2024-04-25 01:15:53 +0530

    Add files via upload

---
+++ Numpy/filter_copy.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 25 01:13:37 2024
+
+@author: Nishant
+"""
+
+"""
+
+Which of the following will return a deep copy of array?
+
+arr = np.array([1,2,3,0,-2,4])
+A. arr1 = arr*1
+
+B. arr1 = arr[:]
+
+C. arr1 = arr[arr > 0]
+
+D. arr1 = arr.reshape(2,3)
+"""
