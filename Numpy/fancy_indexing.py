e290945ac6ffdb0547d6b3ab3253fea584016d96
nishanto95 <167629690+nishanto95@users.noreply.github.com>
2024-04-24 17:11:42 +0530

    Add files via upload

---
+++ Numpy/fancy_indexing.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 24 17:09:22 2024
+
+@author: Nishant
+"""
+
+"""
+import numpy as np
+X = np.arange(12).reshape((3, 4))
+row = np.array([0, 1, 2])
+mask = np.array([1, 0, 1, 0], dtype=bool)
+print(X[row[:, np.newaxis], mask])
+What is the output of the above code?
+"""
+[0,2],[4,6],[8,10]
