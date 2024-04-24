96e29825a8283e7f7127089e613b8d8699843eb3
nishanto95 <167629690+nishanto95@users.noreply.github.com>
2024-04-24 14:54:44 +0530

    Add files via upload

---
+++ Numpy/solution of architecture.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 24 14:51:15 2024
+
+@author: Nishant
+"""
+
+"""
+rnn
+Given the above architecture, where "+", "F1" and "F2" represent
+ element-wise operations.
+
+Suppose an array 'A' of shape (10,3,32) and array 'B' of shape (10,3,1) ar
+e given as input to the unit, what will be the shape of 'X' and 'Y'?
+"""
+x.shape=[10,3,32]
+y.shape=[10,3,1]
